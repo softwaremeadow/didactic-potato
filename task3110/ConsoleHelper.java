8f16169006b9a6c50feb06c85b38360e50275880
softwaremeadow <49923044+softwaremeadow@users.noreply.github.com>
2019-12-15 16:03:15 -0500

    Add files via upload

---
+++ task3110/ConsoleHelper.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        String text = bufferedReader.readLine();
+        return text;
+    }
+
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text.trim());
+    }
+}
